14eb0b6fde402baccbd8b8680bab087201b1793f
eileen <emcnaughton@wikimedia.org>
2020-11-29 11:01:09 +1300

    Code formatting

--- contacteditor.php
+++ contacteditor.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'contacteditor.civix.php';
+
 use CRM_Contacteditor_ExtensionUtil as E;
 
 /**
@@ -9,7 +10,11 @@ use CRM_Contacteditor_ExtensionUtil as E;
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
 function contacteditor_civicrm_config(&$config) {
-  Civi::dispatcher()->addListener('civi.api.prepare', array('CRM_Contacteditor_ChangeContactType', 'validate'), -100);
+  Civi::dispatcher()
+    ->addListener('civi.api.prepare', [
+      'CRM_Contacteditor_ChangeContactType',
+      'validate',
+    ], -100);
   _contacteditor_civix_civicrm_config($config);
 }
 
@@ -130,22 +135,22 @@ function contacteditor_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  * @param array $permissions
  */
 function contacteditor_civicrm_permission(&$permissions) {
-  $permissions['Change CiviCRM contact type'] = array(
+  $permissions['Change CiviCRM contact type'] = [
     E::ts('Change CiviCRM contact type'),
     E::ts('Permits changing a contact type. This may result in data loss if the new type does not support all data of the old type.'),
-  );
+  ];
 }
 
 function contacteditor_civicrm_summaryActions(&$actions, $contactID) {
   if (CRM_Core_Permission::check('Change CiviCRM contact type')) {
-    $actions['casework'] = array(
+    $actions['casework'] = [
       'title' => 'Change Contact Type',
       'weight' => 999,
       'class' => 'no-popup',
       'ref' => 'change-contact-type',
       'key' => 'change-contact-type',
       'href' => CRM_Utils_System::url('civicrm/contacttypechange', ['contact_id' => $contactID]),
-    );
+    ];
   }
 }
 
@@ -156,23 +161,23 @@ function contacteditor_civicrm_summaryActions(&$actions, $contactID) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function contacteditor_civicrm_preProcess($formName, &$form) {
-
-} // */
+ * function contacteditor_civicrm_preProcess($formName, &$form) {
+ *
+ * } // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
  *
-function contacteditor_civicrm_navigationMenu(&$menu) {
-  _contacteditor_civix_insert_navigation_menu($menu, NULL, array(
-    'label' => E::ts('The Page'),
-    'name' => 'the_page',
-    'url' => 'civicrm/the-page',
-    'permission' => 'access CiviReport,access CiviContribute',
-    'operator' => 'OR',
-    'separator' => 0,
-  ));
-  _contacteditor_civix_navigationMenu($menu);
-} // */
+ * function contacteditor_civicrm_navigationMenu(&$menu) {
+ * _contacteditor_civix_insert_navigation_menu($menu, NULL, array(
+ * 'label' => E::ts('The Page'),
+ * 'name' => 'the_page',
+ * 'url' => 'civicrm/the-page',
+ * 'permission' => 'access CiviReport,access CiviContribute',
+ * 'operator' => 'OR',
+ * 'separator' => 0,
+ * ));
+ * _contacteditor_civix_navigationMenu($menu);
+ * } // */
