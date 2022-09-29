b6bd39825681ce7f8e5109c25cf62ce54df0546d
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-09-29 17:16:37 +1300

    Run civix update, tag version

--- contacteditor.php
+++ contacteditor.php
@@ -19,15 +19,6 @@ function contacteditor_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function contacteditor_civicrm_xmlMenu(&$files) {
-  _contacteditor_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -82,54 +73,6 @@ function contacteditor_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function contacteditor_civicrm_managed(&$entities) {
-  _contacteditor_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function contacteditor_civicrm_caseTypes(&$caseTypes) {
-  _contacteditor_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function contacteditor_civicrm_angularModules(&$angularModules) {
-  _contacteditor_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function contacteditor_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _contacteditor_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Add permission to change contact type.
  *
  * @param array $permissions
@@ -181,3 +124,12 @@ function contacteditor_civicrm_summaryActions(&$actions, $contactID) {
  * ));
  * _contacteditor_civix_navigationMenu($menu);
  * } // */
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function contacteditor_civicrm_entityTypes(&$entityTypes) {
+  _contacteditor_civix_civicrm_entityTypes($entityTypes);
+}
